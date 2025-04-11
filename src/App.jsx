a03965357e6f43ff1fee072760a949d55daf1a06
Vedang <modivedang.mv94@gmail.com>
2025-04-11 12:07:55 +0530

    Added SignIn and SignUp components, modified App and firebase.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignOffDashboard from './SignOffDashboard';
 import TeamSelection from './teamSelectionPage'; // Make sure this component exists
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TeamSelection />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/team-selection" element={<TeamSelection />} /> 
         <Route path="/dashboard/:teamName" element={<SignOffDashboard />} />
       </Routes>
     </Router>
